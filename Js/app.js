f2688055e82965e0a7117decdc6ff405057e85f9
Ammaro173 <ammaraf97@gmail.com>
2022-01-23 23:29:07 +0200

    adding js2

--- Js/app.js
+++ Js/app.js
@@ -1,13 +1,11 @@
-var name = prompt('Kindly enter your name' );
+// starting by declaring variables
+'use strict'
+var name = prompt ('Kindly enter your name');
 
-var gender = prompt('Kindly enter your gender [male/female]').toLowerCase();
-
+var gender = prompt ('Kindly enter your gender [male/female]').toLowerCase();
 
 var age = prompt('Kindly insert your age');
-
-var aged =parseInt(age);
-
-if (aged <= 0){
+if (age <= 0){
     alert('age is less or equal to zero');
     age=prompt('age entry is Wrong, Enter Again!');
     if(age <=0){
@@ -15,7 +13,45 @@ if (aged <= 0){
     }
 };
 
+// declaring array for task 6 (questions and answers)
+let ques=['is this your first time visit this site? (Yes/No) Only' , 'would you like to rate us? (Yes/No) Only' , 'do u like to be in the beta team? (Yes/No) Only']
+let ans= new Array(3) //ans : answers
+if(Object.seal){
+    // fill array with some value because
+    // empty slots can not be changed after calling Object.seal
+ans.fill(undefined);
 
+Object.seal(ans);
+// now a is a fixed-size array with mutable entries
+}
+
+for (let index = 0; index < ques.length; index++) {
+    ans[index] = prompt(ques[index]).toLowerCase();
+
+    if (ans[index] === ''){
+        alert('invalid entry , Only Yes/No')
+        ans[index] ='invalid';
+    }
+    else if  (ans[index] !== 'yes' && ans[index] !== 'no') {
+        alert('invalid entry , Only Yes/No')
+        ans[index] ='invalid';
+    }
+    console.log(ans);
+}
+
+function traverse (){
+    console.log(ans);
+    return ans;
+}
+traverse()
+
+// function AgeEntry(){
+// let aged =parseInt(age);
+// }
+// Alert message on wrong age entry
+
+
+// confrim skipping
 let y = confirm('are you sure you want to Skip! the welcome inro? [OK button will Skip ! ]');
 
 if (y == 1){
@@ -38,6 +74,7 @@ else if  (gender == 'male'){
     }
 };
 
+
                                                                  // name = 'Mr.'+ name
                                                                    // var final = alert ('Hello ' + name);
         
@@ -48,7 +85,7 @@ else if  (gender == 'male'){
     //     var final =alert ('Hello ' + name)
     
     
-  
+
 
 
 // console.log(gender);
